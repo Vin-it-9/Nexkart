60af8b621e62e3b15bfbe01ea182a3252ffcb08e
Vin-it-9 <vineetshinde2559@gmail.com>
2025-02-11 19:07:43 +0530

    Commit

--- NexkartFrontend/src/main/java/org/nexus/nexkartfrontend/MainController.java
+++ NexkartFrontend/src/main/java/org/nexus/nexkartfrontend/MainController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class Controllerr {
+public class MainController {
 
     @GetMapping("/")
     public String index() {
