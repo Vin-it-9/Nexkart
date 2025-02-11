d9874ab05e581c26dd4547cb38647c828564cd2b
Vin-it-9 <vineetshinde2559@gmail.com>
2025-02-11 17:15:44 +0530

    Commit

--- NexkartFrontend/src/main/java/org/nexus/nexkartfrontend/NexkartFrontendApplication.java
+++ NexkartFrontend/src/main/java/org/nexus/nexkartfrontend/NexkartFrontendApplication.java
@@ -8,6 +8,11 @@ public class NexkartFrontendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NexkartFrontendApplication.class, args);
+
+        System.out.println("NexkartBackendApplication started");
+        System.out.println("http://localhost:8081/");
+
+
     }
 
 }
