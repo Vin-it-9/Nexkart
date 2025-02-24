b8afd5f705f1a9c6a7cb6f0e88205f56aea438ab
Vin-it-9 <vineetshinde2559@gmail.com>
2025-02-24 15:48:10 +0530

    commit

--- NexkartBackend/src/main/resources/static/js/order_form_tracks.js
+++ NexkartBackend/src/main/resources/static/js/order_form_tracks.js
@@ -15,17 +15,16 @@ $(document).ready(function() {
     });
 
     $("#trackList").on("change", ".dropDownStatus", function(e) {
-        dropDownList = $(this);
-        rowNumber = dropDownList.attr("rowNumber");
-        selectedOption = $("option:selected", dropDownList);
-
-        defaultNote = selectedOption.attr("defaultDescription");
+        var dropDownList = $(this);
+        var rowNumber = dropDownList.attr("rowNumber");
+        var selectedOption = $("option:selected", dropDownList);
+        var defaultNote = selectedOption.attr("defaultDescription");
         $("#trackNote" + rowNumber).text(defaultNote);
     });
 });
 
 function deleteTrack(link) {
-    rowNumber = link.attr('rowNumber');
+    var rowNumber = link.attr('rowNumber');
     $("#rowTrack" + rowNumber).remove();
     $("#emptyLine" + rowNumber).remove();
 }
@@ -37,38 +36,40 @@ function updateTrackCountNumbers() {
 }
 
 function addNewTrackRecord() {
-    htmlCode = generateTrackCode();
+    var htmlCode = generateTrackCode();
     $("#trackList").append(htmlCode);
 }
 
 function generateTrackCode() {
-    nextCount = trackRecordCount + 1;
+    var nextCount = trackRecordCount + 1;
     trackRecordCount++;
-    rowId = "rowTrack" + nextCount;
-    emptyLineId = "emptyLine" + nextCount;
-    trackNoteId = "trackNote" + nextCount;
-    currentDateTime = formatCurrentDateTime();
+    var rowId = "rowTrack" + nextCount;
+    var emptyLineId = "emptyLine" + nextCount;
+    var trackNoteId = "trackNote" + nextCount;
+    var currentDateTime = formatCurrentDateTime();
 
-    htmlCode = `
-			<div class="row border rounded p-1" id="${rowId}">
+    var htmlCode = `
+			<div class="flex flex-col md:flex-row items-start border rounded p-2" id="${rowId}">
 				<input type="hidden" name="trackId" value="0" class="hiddenTrackId" />
-				<div class="col-2">
-					<div class="divCountTrack">${nextCount}</div>
-					<div class="mt-1"><a class="fas fa-trash icon-dark linkRemoveTrack" href="" rowNumber="${nextCount}"></a></div>					
+				<div class="w-full md:w-1/6 flex flex-col items-center">
+					<div class="divCountTrack text-lg font-bold">${nextCount}</div>
+					<div class="mt-1">
+                        <a class="fas fa-trash text-gray-600 hover:text-red-500 linkRemoveTrack" href="" rowNumber="${nextCount}"></a>
+                    </div>					
 				</div>				
 				
-				<div class="col-10">
-				  <div class="form-group row">
-				    <label class="col-form-label">Time:</label>
-				    <div class="col">
-						<input type="datetime-local" name="trackDate" value="${currentDateTime}" class="form-control" required
+				<div class="w-full md:w-5/6">
+				  <div class="flex flex-col md:flex-row items-center mb-2">
+				    <label class="block text-sm font-medium text-gray-700 mr-2">Time:</label>
+				    <div class="flex-grow">
+						<input type="datetime-local" name="trackDate" value="${currentDateTime}" class="border rounded px-2 py-1 w-full max-w-xs" required
 							style="max-width: 300px"/>						
 				    </div>
 				  </div>					
-				<div class="form-group row">  
-				<label class="col-form-label">Status:</label>
-				<div class="col">
-					<select name="trackStatus" class="form-control dropDownStatus" required style="max-width: 150px" rowNumber="${nextCount}">
+				  <div class="flex flex-col md:flex-row items-center mb-2">  
+				    <label class="block text-sm font-medium text-gray-700 mr-2">Status:</label>
+				    <div class="flex-grow">
+						<select name="trackStatus" class="dropDownStatus border rounded px-2 py-1 w-full max-w-xs" required style="max-width: 150px" rowNumber="${nextCount}">
 			`;
 
     htmlCode += $("#trackStatusOptions").clone().html();
@@ -77,37 +78,34 @@ function generateTrackCode() {
 				      </select>						
 				    </div>
 				  </div>
-				  <div class="form-group row">
-				    <label class="col-form-label">Notes:</label>
-				    <div class="col">
-						<textarea rows="2" cols="10" class="form-control" name="trackNotes" id="${trackNoteId}" style="max-width: 300px" required></textarea>
+				  <div class="flex flex-col md:flex-row items-start mb-2">
+				    <label class="block text-sm font-medium text-gray-700 mr-2">Notes:</label>
+				    <div class="flex-grow">
+						<textarea rows="2" cols="10" class="border rounded px-2 py-1 w-full max-w-xs" name="trackNotes" id="${trackNoteId}" style="max-width: 300px" required></textarea>
 				    </div>
 				  </div>
-				  
 				</div>				
 			</div>	
-			<div id="${emptyLineId}" class="row">&nbsp;</div>
+			<div id="${emptyLineId}" class="block">&nbsp;</div>
 	`;
 
     return htmlCode;
 }
 
 function formatCurrentDateTime() {
-    date = new Date();
-    year = date.getFullYear();
-    month = date.getMonth() + 1;
-    day = date.getDate();
-    hour = date.getHours();
-    minute = date.getMinutes();
-    second = date.getSeconds();
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var hour = date.getHours();
+    var minute = date.getMinutes();
+    var second = date.getSeconds();
 
     if (month < 10) month = "0" + month;
     if (day < 10) day = "0" + day;
-
     if (hour < 10) hour = "0" + hour;
     if (minute < 10) minute = "0" + minute;
     if (second < 10) second = "0" + second;
 
     return year + "-" + month + "-" + day + "T" + hour + ":" + minute + ":" + second;
-
 }
